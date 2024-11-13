c5cca3058e24b8dd58f06aad9bd860c2706159a4
Olga2344 <arriba-en@ya.ru>
2024-11-13 14:40:08 +0300

    Доработка с отключением музыки

--- 8_3_Donskova.py
+++ 8_3_Donskova.py
@@ -6,6 +6,8 @@ import time
 import pygame
 
 t=0
+music=False
+
 def set():
     global t
     rem=sd.askstring('time', "input time in format hh.mm")
@@ -14,11 +16,10 @@ def set():
             hour=int(rem.split(':')[0])
             minuts=int(rem.split(':')[1])
             now=datetime.datetime.now()
-            print(now)
-            dt=now.replace(hour=hour, minute=minuts)
-            print(dt)
+            dt=now.replace(hour=hour, minute=minuts, second=0, microsecond=0)
             t=dt.timestamp()
-            print(t)
+            label.config(text=f"reminder: {hour:02}:{minuts:02}")
+
         except Exception as e:
             mb.showerror('error', 'error')
 
@@ -30,12 +31,28 @@ def check():
             play_sound()
             t=0
     window.after(10000,check)
+def stop_m():
+    global music
+    if music:
+        pygame.mixer.music.stop()
+        music=False
+    label.config(text="set new reminder")
 
+def play_sound():
+    global music
+    music=True
+    pygame.mixer.init()
+    pygame.mixer.music.load('r.mp3')
+    pygame.mixer.music.play()
 
 window=Tk()
 window.title("reminder")
 label=Label(text="do reminder")
 label.pack(pady=10)
 set_button=Button(text="do reminder", command=set)
-set_button.pack()
+set_button.pack(pady=10)
+stop_b=Button(text="stop music", command=stop_m)
+stop_b.pack(pady=10)
+
+check()
 window.mainloop()
